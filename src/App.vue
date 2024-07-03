abea70e9cc08186e2704b7fb5227a1842fc09361
irfan juliana winandar <irfanjulianawinandar@gmail.com>
2024-07-03 22:39:44 +0700

    form modal

--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,13 @@
         </div>
       </div>
     </div>
+    <div class="form-overlay">
+      <div class="form-modal">
+        <button class="form-close-btn">&times;</button>
+        <textarea name="memo" id="memo" cols="30" rows="10"></textarea>
+        <button class="form-save-btn">Save</button>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -95,5 +102,52 @@ header {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+}
+
+.form-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.form-modal {
+  width: 420px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-save-btn {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: #495a7d;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-top: 15px;
+  color: white;
+}
+
+.form-close-btn {
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  width: 30px;
+  right: 30px;
+  background-color: transparent;
+  border: none;
+  font-size: 25px;
+  cursor: pointer;
 }
 </style>
